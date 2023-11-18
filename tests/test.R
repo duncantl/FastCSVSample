7bb10b5399f5553fcd96b8ccdcaf51dea5caf10a
Duncan Temple Lang <duncan@r-project.org>
2023-11-18 15:42:37 -0800

    test uses package code

--- tests/test.R
+++ tests/test.R
@@ -1,5 +1,6 @@
-dyn.load("csvSampler.so")
-source("csvSample.R")
+#dyn.load("csvSampler.so")
+#source("csvSample.R")
+library(FastCSVSample)
 
-stopifnot(identical(readLines("mtcars.csv")[c(2, 4, 8) + 1], readLines("mtcars.csv")[c(2, 4, 8) + 1]))
+#stopifnot(identical(readLines("mtcars.csv")[c(2, 4, 8) + 1], readLines("mtcars.csv")[c(2, 4, 8) + 1]))
 
